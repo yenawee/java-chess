ea9909487b8050e1f44b5a5eaf6dc6438aef4b3b
yenawee <yaena0319@naver.com>
2023-04-08 23:32:58 +0900

    fix : table 이름 잘못변경한거 수정

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,15 +1,15 @@
-CREATE TABLE chess_board (
+CREATE TABLE chess_room (
     id TINYINT AUTO_INCREMENT,
     turn VARCHAR(255) NOT NULL,
     state VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE chess_game (
+CREATE TABLE chess_board (
     game_id TINYINT,
     piece_type VARCHAR(255) NOT NULL,
     piece_rank TINYINT NOT NULL,
     piece_file VARCHAR(255) NOT NULL,
     team VARCHAR(255) NOT NULL,
-    FOREIGN KEY (game_id) REFERENCES chess_board(id)
+    FOREIGN KEY (game_id) REFERENCES chess_room(id)
 );
