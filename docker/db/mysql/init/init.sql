cab2c869db4d9e3790fbabeaf1e89cc52eb1601c
yenawee <yaena0319@naver.com>
2023-04-08 20:42:24 +0900

    feat : chessgame table 에서 turn 삭제

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,7 +1,15 @@
+CREATE TABLE chess_room (
+    id TINYINT AUTO_INCREMENT,
+    turn VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE chess_game (
+    game_id TINYINT,
     piece_type VARCHAR(255) NOT NULL,
-    piece_rank TINYINT(10) NOT NULL,
+    piece_rank TINYINT NOT NULL,
     piece_file VARCHAR(255) NOT NULL,
     team VARCHAR(255) NOT NULL,
-    turn VARCHAR(255) NOT NULL
-)
+    FOREIGN KEY (game_id) REFERENCES chess_room(id)
+);
